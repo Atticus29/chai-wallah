71deb5ccbe0846c96bbb4328739f66efe9c879cd
Mark <mark.aaron.fisher@gmail.com>
2017-05-04 10:19:12 -0700

    add display of all of the chais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome!';
+  jugs: Jug[] = [
+    new Jug("Redbush", "Dragonfly", 9.99, "Rooibos"),
+    new Jug("Green Lady", "Dragonfly", 9.99, "Green Tea"),
+    new Jug("Matélatte", "Dragonfly", 9.99, "Maté"),
+    new Jug("Sweet Cheek", "Dragonfly", 9.99, "Sweet"),
+    new Jug("Aztec", "Dragonfly", 9.99, "Spicy"),
+    new Jug("Hansel and Gretl", "Dragonfly", 9.99, "Gingerbread")
+  ]
+}
+
+export class Jug {
+  public dateAdded: Date = new Date();
+  public volume: number = 64;
+  constructor(public name: string, public brand: string, public price: number, public flavor: string) { }
 }
--- src/app/jug/jug.component.ts
+++ src/app/jug/jug.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-jug',
   templateUrl: './jug.component.html',
   styleUrls: ['./jug.component.css']
 })
-export class JugComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class JugComponent{
 
 }
